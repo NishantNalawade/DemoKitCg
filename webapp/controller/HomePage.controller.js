7cdc68555dd794cf83ea7be1e0f2de9585645c38
I339164 <parmeet.singh@sap.com>
2018-04-24 08:07:56 +0000

    initial commit

---
+++ webapp/controller/HomePage.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("com.sap.cg.demokit.DemoKitCg.controller.HomePage", {
+
+	});
+});
