952156aed2aef05103e2016d7becc064fb9d4778
I338015 <amiya.ranjan.das@sap.com>
2018-04-26 06:17:36 +0000

    new list view

--- webapp/controller/DeviceOnboard.controller.js
+++ webapp/controller/DeviceOnboard.controller.js
@@ -1,36 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"com/sap/cg/demokit/DemoKitCg/model/formatter"
-], function(Controller,formatter) {
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
 	"use strict";
 
-	return Controller.extend("com.sap.cg.demokit.DemoKitCg.controller.TenantManager", {
+	return Controller.extend("com.sap.cg.demokit.DemoKitCg.controller.DeviceOnboard", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.TenantManager
+		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.DeviceOnboard
 		 */
-			formatter:formatter,
-			onInit: function() {
-				var oModel=new sap.ui.model.json.JSONModel();
-				$.ajax({
-					url:"model/tenant.json",
-					method:"GET",
-					success : function(data) {
-						oModel.setData(data);
-					},
-					error : function(e) {
-						//error code
-					}
-				});
-				this.getView().setModel(oModel);
-			}
+		//	onInit: function() {
+		//
+		//	},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.TenantManager
+		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.DeviceOnboard
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -39,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.TenantManager
+		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.DeviceOnboard
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -47,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.TenantManager
+		 * @memberOf com.sap.cg.demokit.DemoKitCg.view.DeviceOnboard
 		 */
 		//	onExit: function() {
 		//
